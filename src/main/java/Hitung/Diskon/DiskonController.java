e2b1272fa2584bc8bb10df61b5815058c7962225
khansalsabila <lathifahks1702@gmail.com>
2022-10-30 21:28:58 +0700

    third commit

--- src/main/java/Hitung/Diskon/DiskonController.java
+++ src/main/java/Hitung/Diskon/DiskonController.java
@@ -15,40 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class DiskonController {
-    
-    @RequestMapping("/bayar")
-    public String notamakanan(HttpServletRequest data, Model sayur){
+    Menucamilan Mc = new Menucamilan();
+    @RequestMapping("/submit")
                 
-        Menucamilan Mc = new Menucamilan();
         
-        String nsayur = data.getParameter("var_namasayur");
-        String hsayur = data.getParameter("var_hargakilo");
-        String jsayur = data.getParameter("var_jumlahbeli");
-        String uangbayar = data.getParameter("var_bayarsayur");
+      public String getHasil(HttpServletRequest data, Model model) {
+        String getNama = data.getParameter("Nama_Kue");
+        int getJumlah = Integer.parseInt(data.getParameter("Jumlah_Kue"));
+        int getHarga = Integer.parseInt(data.getParameter("Harga_Kue"));
         
+        int diskon = Mc.getharga(getJumlah, getHarga);
+        int compute = Mc.gethitung(getJumlah, getHarga);
         
-        Double hargasayur = Mc.getharga(hsayur);
-        Double jumlahsayur = Mc.getjumlah(jsayur);
-        Double jumlahbayar = Mc.getjumlahbayar(hargasayur, jumlahsayur);
-        String disc = Mc.getdisc(jumlahbayar);
-        Double hargadiskon = Mc.gethargadisc(jumlahbayar, Integer.valueOf(disc));
-        Double totalbayar =  Mc.gettotalbayar(jumlahbayar, hargadiskon);
-        Double pembayaran = Mc.getpembayaran(uangbayar);
-        Double kembalian = Mc.getkembalian(totalbayar,pembayaran);
-        Mc.gettdiskon(totalbayar, jumlahbayar, hargasayur, Integer.valueOf(disc));
+        model.addAttribute("Nama",getNama);
+        model.addAttribute("Barang", getJumlah);
+        model.addAttribute("Harga", ("Rp. "+ getHarga));
+        model.addAttribute("Total", ("Rp. "+(getHarga*getJumlah)));
+        model.addAttribute("Diskon", "Rp. "+ compute);
+        model.addAttribute("TotalDiskon", diskon + " %");
         
-        
-        
-        sayur.addAttribute("name",nsayur);
-        sayur.addAttribute("price",hsayur);
-        sayur.addAttribute("kilo",jsayur);
-        sayur.addAttribute("tbayar",totalbayar);
-        sayur.addAttribute("hargadisc",hargadiskon);
-        sayur.addAttribute("disc",disc);
-        sayur.addAttribute("total10",jumlahbayar);
-        sayur.addAttribute("jumlah",pembayaran);
-        sayur.addAttribute("kembali",kembalian);
-        return "camilan";
+        return "tableviewer";
     }
     
 }
--- src/main/java/Hitung/Diskon/Menucamilan.java
+++ src/main/java/Hitung/Diskon/Menucamilan.java
@@ -4,87 +4,43 @@
  */
 package Hitung.Diskon;
 
+import org.springframework.stereotype.Controller;
+
 /**
  *
  * @author USER DJOGJA
  */
+@Controller
 public class Menucamilan {
-     public Double getharga(String hsayur){
-       
-        Double hargasayur = Double.valueOf(hsayur);
+     public int gethitung(int jumlah, int harga) {
+        int hargatotal;
+        int total = harga*jumlah;
         
-        return hargasayur;
-    }
-    
-    public Double getjumlah(String jsayur){
-    
-        Double jumlahsayur = Double.valueOf(jsayur);
-    
-        return jumlahsayur;
-    }
-    
-    public Double gettotalbayar(Double jbayar, Double hdiskon){
-    
-        Double tbayar = jbayar - hdiskon;
-        
-        return tbayar;
-    }
-    
-    public Double getjumlahbayar(Double hsayur, Double jsayur){
-    
-        Double jbayar = hsayur * jsayur;
-    
-        return jbayar;
-    }
-    
-    public String getdisc(Double jbayar){
-    
-        String disc = null;
-        
-        if (jbayar <16000) {
-            disc = "0";
-        }else if (jbayar <25000){
-            disc = "10";
-        }else{
-            disc = "15";
+        if(total<10000){
+            hargatotal = total - (total * 0);
         }
-        return disc;
-    }
-    
-    public Double gethargadisc(Double jbayar, Integer disk){
-        
-        Double hdiskon = jbayar*disk/100;
-    
-        return hdiskon;
-    }
-    
-    public void gettdiskon(Double tbayar, Double jumbayar, Double hdiskon, Integer diskon){
-        
-        if(jumbayar < 16000){
-            diskon = 0;
-            tbayar = jumbayar - (jumbayar*diskon/100);
-            hdiskon = jumbayar*diskon/100;
-            
-        }else if( jumbayar < 25000){
-            diskon = 10;
-            tbayar = jumbayar - (jumbayar*diskon/100);
-            hdiskon = jumbayar*diskon/100;
-        }else {
-            diskon = 15;
-            tbayar = jumbayar - (jumbayar*diskon/100);
-            hdiskon = jumbayar*diskon/100;
+        else if ((total > 10000) && (total < 50000)){
+            hargatotal = total - (total * 5/100);
         }
+        else{
+            hargatotal = total - (total * 10/100);
+        }
+        return hargatotal;
     }
     
-    public Double getpembayaran (String uangbayar) {
-        Double pembayaran = Double.valueOf(uangbayar);
+    public int getharga(int jumlah, int harga){
+        int total = harga*jumlah;
+        int totalDiskon;
         
-        return pembayaran;
-    }
-    
-    public Double getkembalian(Double tbayar,Double uangbayar){
-        Double uangkembalian = uangbayar - tbayar;
-        
-        return uangkembalian;
+        if(total < 10000){
+            totalDiskon = 0;
+        }
+        else if((total > 10000)&&(total < 50000)){
+            totalDiskon = 5;
+        }
+        else{
+            totalDiskon = 10;
+        }
+        return totalDiskon;
     }
 }
